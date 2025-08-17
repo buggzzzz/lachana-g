0026139607aa48a07fc96e4dcec229f7fa71218f
buggzzzz <themadpunisher6@gmail.com>
2025-08-17 15:10:05 +0300

    linked the rooms

--- app-files/data.js
+++ app-files/data.js
@@ -24,7 +24,32 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.905224483279671,
+          "pitch": 0.17898044758104348,
+          "rotation": 0,
+          "target": "8-hall"
+        },
+        {
+          "yaw": -0.9101325551443971,
+          "pitch": 0.12380602250710915,
+          "rotation": 0,
+          "target": "3-bedroom-1"
+        },
+        {
+          "yaw": -0.39726645701227525,
+          "pitch": 0.15370012547416323,
+          "rotation": 0,
+          "target": "4-bedroom-2"
+        },
+        {
+          "yaw": 2.446770972646151,
+          "pitch": -0.023818389948855767,
+          "rotation": 0,
+          "target": "9-building-hall-g-floor"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -51,7 +76,20 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.6740368135633936,
+          "pitch": 0.04821248453609783,
+          "rotation": 0,
+          "target": "5-b-yard-1"
+        },
+        {
+          "yaw": -2.039299324023137,
+          "pitch": 0.04001944043445249,
+          "rotation": 0,
+          "target": "8-hall"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -78,7 +116,14 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -2.081344048657993,
+          "pitch": 0.09547589920273403,
+          "rotation": 0,
+          "target": "8-hall"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -105,7 +150,20 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.4665146985593829,
+          "pitch": 0.0721535421455819,
+          "rotation": 0,
+          "target": "6-b-yard-2"
+        },
+        {
+          "yaw": 2.6915410744272057,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -132,7 +190,20 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.383740606471129,
+          "pitch": 0.061137009439867995,
+          "rotation": 0,
+          "target": "7-b-yard-3"
+        },
+        {
+          "yaw": -0.080836549834709,
+          "pitch": 0.06062209301302701,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -159,7 +230,26 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.5051327704258242,
+          "pitch": -0.01474097517568751,
+          "rotation": 0,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": -0.011675908336016505,
+          "pitch": 0.048330123727215124,
+          "rotation": 0,
+          "target": "6-b-yard-2"
+        },
+        {
+          "yaw": 1.1005399654447459,
+          "pitch": 0.031148216122426575,
+          "rotation": 0,
+          "target": "7-b-yard-3"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -186,7 +276,26 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.7145351712112067,
+          "pitch": 0.021378827302392267,
+          "rotation": 0,
+          "target": "5-b-yard-1"
+        },
+        {
+          "yaw": 0.8410545927759792,
+          "pitch": 0.038513328596220475,
+          "rotation": 0,
+          "target": "3-bedroom-1"
+        },
+        {
+          "yaw": 0.04205281329592481,
+          "pitch": -0.008367351275602175,
+          "rotation": 0,
+          "target": "7-b-yard-3"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -213,7 +322,32 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.3252499348345825,
+          "pitch": 0.004386974739901461,
+          "rotation": 0,
+          "target": "5-b-yard-1"
+        },
+        {
+          "yaw": 0.19900083587119966,
+          "pitch": 0.012147060967269141,
+          "rotation": 0,
+          "target": "6-b-yard-2"
+        },
+        {
+          "yaw": -2.196456691285306,
+          "pitch": 0.0160341204767942,
+          "rotation": 0,
+          "target": "4-bedroom-2"
+        },
+        {
+          "yaw": -0.8040710164667821,
+          "pitch": 0.0731891925122774,
+          "rotation": 0,
+          "target": "3-bedroom-1"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -240,7 +374,60 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.7695171957993914,
+          "pitch": 0.06462895028726656,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 0.09014276183839343,
+          "pitch": 0.04385803542182032,
+          "rotation": 0,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": 2.0275019259066855,
+          "pitch": 0.05989215589861274,
+          "rotation": 0,
+          "target": "2-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-building-hall-g-floor",
+      "name": "Building Hall G Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05871266648994933,
+          "pitch": 0.05837319104075611,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
       "infoHotspots": []
     }
   ],
